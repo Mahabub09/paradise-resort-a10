3fd882300185a5c25e6d70ff2fa63f929af30c5b
mahbub09 <mahabub895alam@gmail.com>
2022-04-18 23:07:28 +0600

    halka

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
             <img className='w-25' src={mahabub} alt="" />
             <h2>Mahabub Alam</h2>
 
-            <p className='w-50'>First of all i want to be a Full Stack Web Developer . I have no interest in Programming but i have plenty ideas and a big dream for  become a Professional Web Developer. And i am working hard on it to become my dream true. But i know i may have some lakings but i am truly dedicated for improving me to the next level web developer.</p>
+            <p className='w-50'>Hello I am Mahabub Alam . I am a student of Complete Web Development course batch 5 .First of all i want to be a Full Stack Web Developer . I have no interest in Programming but i have plenty ideas and a big dream for  become a Professional Web Developer. And i am working hard on it to become my dream true. But i know i may have some lakings but i am truly dedicated for improving me to the next level web developer.</p>
 
         </div>
     );
